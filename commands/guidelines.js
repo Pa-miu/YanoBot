7fce770b5a2d49fabdd23c6ede9af4008d4c6100
Pa-miu <gachaisbadciv@gmail.com>
2018-04-30 13:13:17 -0400

    Fix syntax typo and message formatting

--- commands/guidelines.js
+++ commands/guidelines.js
@@ -1,14 +1,14 @@
-const Discord = require('dicord.js');
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-	let guidelines = '**Don\'t abuse the bot**\
-			- #bot-jigoku is the anarcho-communist state of channels\
-			- Dont\' spam memes anywhere outside of #bot-jigoku\
-			- Spam is more than 1 meme per 30 seconds, roughly\
-			- If a lot of people are posting memes maybe wait a bit\
-			- If you give yourself a role you leave yourself open to pings\
-			- Don\'t spam role pings, treat them like you would treat @everyone\
-			- Bother @medjed#9811 about bot related stuff';
+	let guidelines = '**Don\'t abuse the bot**\n\
+	- #bot-jigoku is the anarcho-communist state of channels\n\
+	- Dont\' spam memes anywhere outside of #bot-jigoku\n\
+	- Spam is more than 1 meme per 30 seconds, roughly\n\
+	- If a lot of people are posting memes maybe wait a bit\n\
+	- If you give yourself a role you leave yourself open to pings\n\
+	- Don\'t spam role pings, treat them like you would treat @everyone\n\
+	- Bother @medjed#9811 about bot related stuff';
 
 	message.channel.send(guidelines);
 };
