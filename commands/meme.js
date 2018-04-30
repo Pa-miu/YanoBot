9356090fa2abeac394af1003b1b1d16c53c0fc16
Pa-miu <gachaisbadciv@gmail.com>
2018-04-29 22:28:35 -0400

    Consolidated some dev objects to botconfig.json

--- commands/meme.js
+++ commands/meme.js
@@ -1,13 +1,8 @@
 const Discord = require('discord.js');
 const Fuse = require('fuse.js');
-const MEME_DB = require('../memes.json');
-const FUSE_OPTIONS = {
-	shouldSort: true,
-	threshold: 0.3,
-	maxPatternLength: 32,
-	minMatchCharLength: 3,
-	keys: ['meme']
-};
+const botConfig = require('../botconfig.json');
+const MEME_DB = botConfig.memeDB;
+const FUSE_OPTIONS = botConfig.fuseoptions;
 
 module.exports.run = async (bot, message, args) => {
 	let meme = args.join(' ');
